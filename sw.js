e5f0fe82ad63af42dd42a5e45ecbfd801dce7848
infospt <54210883+infospt@users.noreply.github.com>
2026-06-21 16:31:14 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,51 +1,13 @@
-const CACHE_NAME = "ma-liste-epicerie-v7";
-const FILES_TO_CACHE = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icon-180.png",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
 self.addEventListener("install", event => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(names => {
-      return Promise.all(
-        names
-          .filter(name => name !== CACHE_NAME)
-          .map(name => caches.delete(name))
-      );
-    }).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener("fetch", event => {
-  const request = event.request;
-
-  if (request.mode === "navigate" || request.destination === "document") {
-    event.respondWith(
-      fetch(request, { cache: "no-store" })
-        .then(response => {
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put("./index.html", clone));
-          return response;
-        })
-        .catch(() => caches.match("./index.html"))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(request).then(cached => {
-      return cached || fetch(request);
-    })
+    caches.keys()
+      .then(names => Promise.all(names.map(name => caches.delete(name))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.matchAll())
+      .then(clients => Promise.all(clients.map(client => client.navigate(client.url))))
   );
 });
