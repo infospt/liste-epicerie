a1bcbfc86fc55bd05ce594ad683072d2f7a113c4
infospt <54210883+infospt@users.noreply.github.com>
2026-06-22 09:08:03 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+const CACHE_NAME = "ma-liste-epicerie-v9";
+
 self.addEventListener("install", event => {
   self.skipWaiting();
 });
@@ -5,7 +7,7 @@ self.addEventListener("install", event => {
 self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys()
-      .then(names => Promise.all(names.map(name => caches.delete(name))))
+      .then(names => Promise.all(names.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))))
       .then(() => self.registration.unregister())
       .then(() => self.clients.matchAll())
       .then(clients => Promise.all(clients.map(client => client.navigate(client.url))))
