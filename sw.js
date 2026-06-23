2d771e19a85e68fd26cf6915c01888ac9895e4d5
infospt <54210883+infospt@users.noreply.github.com>
2026-06-23 15:48:22 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "ma-liste-epicerie-v60";
+const CACHE_NAME = "ma-liste-epicerie-v61";
 self.addEventListener("install", event => { self.skipWaiting(); });
 self.addEventListener("activate", event => { event.waitUntil((async () => { const keys = await caches.keys(); await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))); await self.clients.claim(); })()); });
 self.addEventListener("fetch", event => { return; });
