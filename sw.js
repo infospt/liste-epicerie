52ce24999e8bf2cc731a2fdc5b3c4ae10cc2dba5
infospt <54210883+infospt@users.noreply.github.com>
2026-06-24 15:16:27 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,25 @@
-const CACHE_NAME = "ma-liste-epicerie-v86";
-self.addEventListener("install", event => { self.skipWaiting(); });
-self.addEventListener("activate", event => { event.waitUntil((async () => { const keys = await caches.keys(); await Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))); await self.clients.claim(); })()); });
-self.addEventListener("fetch", event => { return; });
+const CACHE_NAME = "ma-liste-epicerie-v87";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-180.png",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./version.json"
+];
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
